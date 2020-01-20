8cfec30cbeb07bf4f6eb6ca5c99108735b9e72fd
weinbecher <382316736@qq.com>
2020-01-20 14:06:16 +0000

    inital commit

---
+++ src/test/java/com/codeclan/example/employeeData/EmployeeDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.employeeData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
